ee4120de9a45d96b33255217a028da49a1b17be7
Lucas Oliveira <lucasegurao@gmail.com>
2025-03-20 14:53:16 -0300

    Update script.js

--- ex008/script.js
+++ ex008/script.js
@@ -1,6 +1,5 @@
 var numero1 = Number(prompt("Digite um valor para subtrair:"))
 var numero2 = Number(prompt("Digite um valor para subtrair:"))
-
 var soma = numero1 - numero2
 
 alert ("A subtração de " + numero1 + " x " + numero2 + " é " + soma)
