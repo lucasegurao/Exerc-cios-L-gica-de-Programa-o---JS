e1558cb53cb2ee443abff08bfee4e74e5ea83394
Lucas Oliveira <lucasegurao@gmail.com>
2025-03-20 14:49:41 -0300

    Update script.js

--- ex007/script.js
+++ ex007/script.js
@@ -1,6 +1,5 @@
 var numero1 = Number(prompt("Digite um valor para multiplicar:"))
 var numero2 = Number(prompt("Digite um valor para multiplicar:"))
-
 var soma = numero1 * numero2
 
 alert ("A multiplicação de " + numero1 + " x " + numero2 + " é " + soma)
