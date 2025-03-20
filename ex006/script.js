0928f8a6fb9d9b1970f14d858d44b05417854083
Lucas Oliveira <lucasegurao@gmail.com>
2025-03-20 14:49:27 -0300

    Update script.js

--- ex006/script.js
+++ ex006/script.js
@@ -1,6 +1,5 @@
 var numero1 = Number(prompt("Digite um valor para divisão:"))
 var numero2 = Number(prompt("Digite um valor para divisão:"))
-
 var soma = numero1 / numero2
 
 alert ("A divisão de " + numero1 + " ÷ " + numero2 + " é " + soma)
